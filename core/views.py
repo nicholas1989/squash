aaabae044fb2f2788bcd4684e137e95b9060c83f
[Nkemakolam Nwokoro] <[nwokoronicholas@yahoo.com]>
2021-01-15 16:35:44 +0100

    Working with serializers

--- core/views.py
+++ core/views.py
@@ -1,22 +1,23 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from.serializers import PostSerializer
 
 # Create your views here.
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'Obinna',
-            'age': 33
-        }
-        return Response(data)
+        qs = Post.object.all()
+        post = qs.first()
+        #serializer = PostSerializer(qs, many=True)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
 
 
-
-   #def test_view(request):
-    #   data = {
-    #      'name': 'Obinna',
-    #     'age': 33
-        #}
-        #return JsonResponse(data)
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
